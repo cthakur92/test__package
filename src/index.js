74f1d8b6dc7704baaeabfcacd57e2543ea9f2ee9
Chandan Thakur <chandan.thakur1@hp.com>
2019-03-01 12:25:55 +0530

    -- working image import; jsx/scss to js/css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-import TestComponent from "./test_component/js/container/TestComponent.js";
+import TestComponent from "./test_component/js/container/TestComponent.jsx";
 // export default TestComponent;
 export { TestComponent };
-// exports.TestComponent = require("./test_component/js/container/TestComponent.jsx").TestComponent;
-
-;
-
-var _temp = function () {
-    if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-        return;
-    }
-}();
-
-;
